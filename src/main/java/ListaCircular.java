7f5ea87ba5858fd1042d071985ad9100c74fb53a
Luiz Roberto <luizrobertoao29@gmail.com>
2023-02-24 12:15:21 -0300

    feat: Implementa método remove().

--- src/main/java/ListaCircular.java
+++ src/main/java/ListaCircular.java
@@ -4,15 +4,34 @@ public class ListaCircular<T> {
     private No<T> cauda;
     private int tamanhoLista;
 
+    public void remove(int index) {
+        if (index >= this.tamanhoLista)
+            throw new IndexOutOfBoundsException("O índice é maior que o tamanho da lista.");
+
+        No<T> noAuxiliar = this.cauda;
+        if (index == 0) {
+            this.cauda = this.cauda.getNoProximo();
+            this.cabeca.setNoProximo(this.cauda);
+        } else if (index == 1) {
+            this.cauda.setNoProximo(this.cauda.getNoProximo());
+        } else {
+            for (int i = 0; i < index-1; i++) {
+                noAuxiliar = noAuxiliar.getNoProximo();
+            }
+            noAuxiliar.setNoProximo(noAuxiliar.getNoProximo().getNoProximo());
+        }
+        this.tamanhoLista--;
+    }
+
     public T get(int index) {
         return this.getNo(index).getConteudo();
     }
 
     private No<T> getNo(int index) {
-        if(this.isEmpty())
-            throw  new IndexOutOfBoundsException("A lista está vazia.");
+        if (this.isEmpty())
+            throw new IndexOutOfBoundsException("A lista está vazia.");
 
-        if(index == 0) {
+        if (index == 0) {
             return this.cauda;
         }
         No<T> noAuxiliar = this.cauda;
